8058a097412e48c7494bc2779b5f4a25a02f93c6
Tushar Sharma <tusharsharma783@gmail.com>
2019-02-02 22:43:08 +0530

    Update lwcDataTable.js

--- force-app/main/default/lwc/lwcDataTable/lwcDataTable.js
+++ force-app/main/default/lwc/lwcDataTable/lwcDataTable.js
@@ -10,9 +10,6 @@ export default class LwcDataTable extends LightningElement {
     wiredContacts({data}) {
         if (data) {
             this.data = data.sobList;
-          /*  console.log('-----------------');
-            console.log(data.sobList);
-            console.log(data.ldwList);*/
             this.columns = data.ldwList;
         }
     }
@@ -20,18 +17,9 @@ export default class LwcDataTable extends LightningElement {
     getSelectedName(event) {
         const selectedRows = event.detail.selectedRows;
         if(selectedRows.length > 0) {
-            console.log('----------1214-------'+selectedRows[0].Id);
-
            const selectedEvent = new CustomEvent('selected', { detail: selectedRows[0].Id });
            // Dispatches the event.
            this.dispatchEvent(selectedEvent);
-        }
-        
-        /*
-        // Display that fieldName of the selected rows
-        for (let i = 0; i < selectedRows.length; i++){
-            selectedRows[i].checked= false;
-           // alert("You selected: " + selectedRows[i].Id);
-        }*/
+        }       
     }
 }
