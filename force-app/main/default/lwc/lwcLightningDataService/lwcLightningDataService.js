f1e83818b7479acb26e9cd6c8a77b9fee09e6a9e
Tushar Sharma <tusharsharma783@gmail.com>
2019-02-02 22:43:35 +0530

    Update lwcLightningDataService.js

--- force-app/main/default/lwc/lwcLightningDataService/lwcLightningDataService.js
+++ force-app/main/default/lwc/lwcLightningDataService/lwcLightningDataService.js
@@ -7,15 +7,13 @@ export default class LwcLightningDataService extends LightningElement {
   @api objectApiName = 'Contact';
 
   contactSelected(event) {
-    //console.log('---------------------------->>>> '+event.detail)
     const contactId = event.detail;
     this.recordId = contactId;
     this.showLDS = true;
-    //this.selectedContact = this.contacts.data.find(contact => contact.Id === contactId);
   }
 
   handleSuccess(event) {
-   // this.accountId = event.detail.id;
+   // event.detail.id;
    alert('Record updated successfully.');
 }
 } 
